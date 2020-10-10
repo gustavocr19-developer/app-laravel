d7d7a013b396a5f220c20e290e54a9d6ed14f7e7
Gustavo Camargo Rodrigues <gustavocr.developer@gmail.com>
2020-10-10 16:04:17 -0300

    creating routes

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,1 @@
+<h1>Contato da empresa View</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,76 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+Route::get('/login', function (){
+    return 'login';
+})->name('login');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware(['auth'])->group(function (){
+
+    Route::prefix('admin')->group(function () {
+
+        Route::get('/dashboard', function (){
+            return 'Home Admin';
+        });
+    
+        Route::get('/financeiro', function (){
+            return 'Financeiro Admin';
+        });
+    
+        Route::get('/produtos', function (){
+            return 'Produtos Admin';
+        });
+    }); 
+});
+
+Route::get('/redirect3', function (){
+    return redirect()->route('url.name');
+});
+
+Route::get('/nome-url', function(){
+    return 'hey hey';
+})->name('url.name');
+
+Route::get('/redirect1', function (){
+    return redirect('/redirect2');
+});
+
+Route::get('/redirect2', function(){
+    return 'redirect2';
+});
+
+Route::get('/produtos/{idProduct?}', function ($idProduct = 'dfsddf'){
+    return "Produtos {$idProduct}";
+});
+
+
+Route::get('/categoria/{flag}/posts', function($flag) {
+    return "Posts da categoria: {$flag}";
+});
+
+Route::get('/categorias/{flag}', function($flag) {
+    return "produtos da categoria: {$flag}";
+});
+
+Route::match(['post','get'], 'match', function (){
+    return 'match';
+});
+
+Route::any('/any', function (){
+    return 'any';
+});
+
+Route::post('/register', function(){
+    return 'register';
+});
+
+Route::get('/empresa', function () {
+    return view('site.contato');
+});
+
+Route::get('/contato', function(){
+    return 'contato da empresa';
+});
+
 
 Route::get('/', function () {
     return view('welcome');
